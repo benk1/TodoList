b3c43d6c41e352a12b22b4b127b4ae43d7c8be3b
benk1 <kakengi_bernard@hotmail.com>
2019-01-05 03:46:59 +0200

    first commit

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <React.Fragment>
+        <h1>About</h1>
+        <p>This is Todo App V1.0.0, it is part of React learning Curve</p>
+      
+    </React.Fragment>
+  )
+}
+export default About;
